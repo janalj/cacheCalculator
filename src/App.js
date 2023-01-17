d829cfdb407cfffb99d38491ff7e73d1d6142feb
Luying <44931863+janalj@users.noreply.github.com>
2023-01-16 17:53:29 -0800

    Add frontend Direct Map form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
 import logo from './logo.svg';
+import React, { Component,useState } from 'react';
 import './App.css';
-
+import axios from 'axios';
 function App() {
+
+  // Fix the api 
+
+
+    const api = 'https://7hfkckt5q6.execute-api.us-east-1.amazonaws.com/directMap';
+    const data = { "cacheSize" : "2048" , "blockSize":"32"};
+    axios
+      .post(api, data,{headers: {"Access-Control-Allow-Origin": "https://7hfkckt5q6.execute-api.us-east-1.amazonaws.com/"}})
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+
+  const [cacheSize, setCacheSize] = useState("");
+  const [blockSize, setBlockSize] = useState("");
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Cahce Calculator</h1>
+
+      <h3>Dicreted Map</h3>
+
+      <form>
+      <label>
+        Cahce Size 
+      <input
+        value={cacheSize}
+        onChange={e => setCacheSize(e.target.value)}
+        placeholder="Enter Cache Size"
+        type="text"
+        name="firstName"
+        required
+      />
+      </label>
+      <br/>
+      <label>
+        Block Size
+      <input
+        value={blockSize}
+        onChange={e => setBlockSize(e.target.value)}
+        placeholder="Enter Block Size"
+        type="text"
+        name="lastName"
+        required
+      />
+      </label>
+      <br/>
+      <button type="submit">Calculate</button>
+    </form>
+      <p>Tag</p>
+      <p>Sets</p>
+      <p>Offsets</p>
     </div>
   );
 }
